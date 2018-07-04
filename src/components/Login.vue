c99531a94e140fedaa3e8a411edf8588d3b1b1e5
Alessandro <alessandrolimabcc@gmail.com>
2018-07-04 15:40:53 -0300

    Primeiro commit de novo projeto
    acredito que o eslint n mais atrapalhe nosso desenvolvimento.

---
+++ src/components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+   <b-container class='login'>
+     <b-row>
+       <b-col>
+         <h1>{{msg}}</h1>
+         <b-card header="Bem vindo"
+                class="text-center">
+            <b-form>
+          <b-form-input id='cpf'
+                        type='text'
+                        v-model="form.cpf"
+                        required
+                        placeholder='cpf'>
+          </b-form-input>
+
+          <b-form-input id='senha'
+                        type='password'
+                        v-model="form.senha"
+                        required
+                        placeholder='senha'>
+          </b-form-input>
+
+            <b-button type='submit' variant='primary'>Login</b-button>
+
+        </b-form>
+        </b-card>
+        
+       </b-col>
+     </b-row>
+   </b-container>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'Módulo de Marcação de Consultas do DQV',
+      form: {
+        cpf: '',
+        senha: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.login{
+      background-color: rgba(255, 0, 0, 1);
+      padding-bottom: 20px;
+ }
+</style>
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+
+
+   
+</template>
+
+<script>
+export default {
+   name:'navbar',
+
+}
+</script>
+
+<style>
+
+
+</style>
